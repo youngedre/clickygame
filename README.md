1578006f91e9facac51869e1fb946c07e07b5a11
William Whitted <youngedre@gmail.com>
2019-11-10 23:25:17 -0500

    styling changes as well as readme update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# clickygame
+# Clicky-Game
+
+This is a memory game built using react. This is an app that shows the usage of reacts virtual DOM. The virtual Dom is updating parts of the file that are updated via the state.
+
+*static version, game function still being worked on
+When the finished game is complete: 
+
+Start by clicking an image and see how many you can click without hitting the same image twice. The app randomly cycles through 16 different images. 
+
+## Built with: 
+
+1) [React](https://reactjs.org/docs/getting-started.html)
+2) [JSX](https://reactjs.org/docs/introducing-jsx.html)
+3) [Bootstrap CSS](https://getbootstrap.com/)
--- clickygame/src/components/Jumbotron/index.js
+++ clickygame/src/components/Jumbotron/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 
 const Jumbotron = props => {
     return (
-        <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4 text-center">Hero Clicky Game!</h1>
-          <p class="lead text-center">Click on an image to earn points, but don't click on any more than once!</p>
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center font-weight-bold">Hero Clicky Game!</h1>
+          <p className="lead text-center">Click on an image to earn points, but don't click on any more than once!</p>
         </div>
       </div>
     );
