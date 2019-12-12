d68f2a5dc687ec99540f1ab6d2e89224577a3aac
William Whitted <youngedre@gmail.com>
2019-12-12 11:50:33 -0500

    Updates

---
+++ precache-manifest.f48a0b428b6473dd6a96550a8037d98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381abaed1b7cba02ad2523b81174208c",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "58a0e10bff7f43e1d225",
+    "url": "/clickygame/static/css/main.307b25f6.chunk.css"
+  },
+  {
+    "revision": "deeb7c3a9fe60ed50c2d",
+    "url": "/clickygame/static/js/2.9c7b33a0.chunk.js"
+  },
+  {
+    "revision": "58a0e10bff7f43e1d225",
+    "url": "/clickygame/static/js/main.ea425be1.chunk.js"
+  },
+  {
+    "revision": "6f48ede0d20a77d9f231",
+    "url": "/clickygame/static/js/runtime-main.b8d724e2.js"
+  }
+]);
